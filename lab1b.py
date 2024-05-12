bc511b7535bd4b89b0a467be4fe39772aa4ba42e
Chikong LAU <cklau9@myseneca.ca>
2024-05-11 20:44:22 -0400

    Added lab1b

---
+++ lab1b.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''
+    To gain practice, complete your Python script with the following content and details:
+        The script should have a Shebang line like you did for your lab1a.py python script
+        The script should use a single object called "name"
+        The value of the "name" object should be "Isaac"
+        The script, when executed, should print out "How old are you Isaac?"
+        Sample run:
+
+cd ~/ops445/lab1/
+./lab1b.py
+ 
+How old are you Isaac?
+'''
+
+name = 'Isaac'
+
+print('How old are you ' + name + '?')
